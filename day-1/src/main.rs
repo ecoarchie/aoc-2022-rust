fb43cb01971bdee69bd545b58014aaea020cc0ec
ecoarchie <eco.artur@gmail.com>
2023-08-10 22:52:56 +0300

    day-1 part 1 done

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::read_to_string;
+
+fn read_lines(filename: &str) -> Vec<i32> {
+    read_to_string(filename) 
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(|n| n.parse::<i32>().unwrap_or_default())  // make each slice into a string
+        .collect()  // gather them together into a vector
+}
+
 fn main() {
-    println!("Hello, world!");
+    let vec = read_lines("./input");
+    let mut max = 0;
+    let mut sum = 0;
+    for num in vec {
+        if num > 0 {
+            sum += num;
+        } else {
+            if sum > max {
+                max = sum;
+            }
+            sum = 0;
+        }
+    }
+    println!("{}", max);
 }
